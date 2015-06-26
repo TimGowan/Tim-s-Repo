aac83a1e72ed4eca8bd13b18f56d66bc373668be
TimGowan <txg112130@utdallas.edu>
2015-06-26 04:57:15 -0500

    TestingGoogleSpeech updates
    misc updates

--- TestingGoogleSpeech/app/src/main/java/com/gspeech/tim/testinggooglespeech/MainActivity.java
+++ TestingGoogleSpeech/app/src/main/java/com/gspeech/tim/testinggooglespeech/MainActivity.java
@@ -1,145 +1,59 @@
 package com.gspeech.tim.testinggooglespeech;
 
+import java.util.Locale;
+import java.util.Random;
 
-        import java.util.ArrayList;
-        import java.util.Locale;
+import android.app.Activity;
+import android.os.Bundle;
 
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-        import android.app.Activity;
-        import android.content.ActivityNotFoundException;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.speech.RecognizerIntent;
-        import android.speech.tts.TextToSpeech;
-        import android.util.Log;
-        import android.view.Menu;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import android.speech.tts.TextToSpeech; ////NEED THIS
 
-public class MainActivity extends Activity implements
-        TextToSpeech.OnInitListener {
+public class MainActivity extends Activity {
 
-    private TextView txtSpeechInput;
-    private Button btnSpeak;
-    private final int REQ_CODE_SPEECH_INPUT = 100;
-
-    private TextToSpeech tts;
-    private Button btnListen;
-    private EditText txtText;
-
-
+    TextToSpeech ttobj;
+    private EditText write;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        write = (EditText)findViewById(R.id.editText1);
 
-        tts = new TextToSpeech(this, this);
-        btnSpeak = (Button) findViewById(R.id.btnListen);
-
-
-        txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
-        btnSpeak = (Button) findViewById(R.id.btnSpeak);
-
-        // hide the action bar
-        getActionBar().hide();
-
-        btnSpeak.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                promptSpeechInput();
-            }
-        });
-        // button on click event
-        btnListen.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-                speakOut();
-            }
-
-        });
-
-    }
-
-    /**
-     * Showing google speech input dialog
-     * */
-    private void promptSpeechInput() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
-                getString(R.string.speech_prompt));
-        try {
-            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-        } catch (ActivityNotFoundException a) {
-            Toast.makeText(getApplicationContext(),
-                    getString(R.string.speech_not_supported),
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
-    @Override
-    public void onDestroy() {
-        // Don't forget to shutdown tts!
-        if (tts != null) {
-            tts.stop();
-            tts.shutdown();
-        }
-        super.onDestroy();
+        //***NEED THIS FROM HERE-
+        ttobj=new TextToSpeech(getApplicationContext(),
+                new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if(status != TextToSpeech.ERROR){
+                            ttobj.setLanguage(Locale.US);
+                        }
+                    }
+                });
+        //*** -TO HERE
     }
 
     @Override
-    public void onInit(int status) {
-
-        if (status == TextToSpeech.SUCCESS) {
-
-            int result = tts.setLanguage(Locale.US);
-
-            if (result == TextToSpeech.LANG_MISSING_DATA
-                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.e("TTS", "This Language is not supported");
-            } else {
-                btnSpeak.setEnabled(true);
-                speakOut();
-            }
-
-        } else {
-            Log.e("TTS", "Initilization Failed!");
+    public void onPause(){
+        if(ttobj !=null){
+            ttobj.stop();
+            ttobj.shutdown();
         }
-
+        super.onPause();
     }
 
-    private void speakOut() {
-
-        String text = txtText.getText().toString();
-
-        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    public void speakText(String s){
+        ttobj.speak(s, TextToSpeech.QUEUE_ADD, null);
     }
 
 
-    /**
-     * Receiving speech input
-     * */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case REQ_CODE_SPEECH_INPUT: {
-                if (resultCode == RESULT_OK && null != data) {
-
-                    ArrayList<String> result = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    txtSpeechInput.setText(result.get(0));
-                }
-                break;
-            }
-
-        }
+    public void speakText(View view){
+        String toSpeak = write.getText().toString();
+        Toast.makeText(getApplicationContext(), toSpeak,
+                Toast.LENGTH_SHORT).show();
+        ttobj.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
